8a560d79576145abc2d894821eadc84d4ae7d883
ED1FF <edifei13@yandex.ua>
2018-03-08 10:11:49 +0200

    asd

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :scores
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'scores#index'
+  root 'scores#index'
 end
