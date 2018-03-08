af040739db4dedd29fdee8cf0351301a82f7f933
ED1FF <edifei13@yandex.ua>
2018-03-08 12:20:16 +0200

    registrations added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :scores
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'scores#index'
+  root to: 'scores#index'
 end
